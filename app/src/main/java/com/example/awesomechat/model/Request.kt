8cdd09a40b8725c65ab64c8cfc45a86006df88fd
CommudeVN Nam <nguyenhoainam.090801@gmail.com>
2021-10-11 10:44:26 +0700

    fix data class

--- app/src/main/java/com/example/awesomechat/model/Request.kt
+++ app/src/main/java/com/example/awesomechat/model/Request.kt
@@ -3,15 +3,8 @@ package com.example.awesomechat.model
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-class Request {
-    var userName: String? = null
-    var mailRequest: String? = null
-    var profileImage : String? = null
-
-    constructor(userName: String?, mailRequest: String?,profileImage: String?) {
-        this.userName = userName
-        this.mailRequest = mailRequest
-        this.profileImage = profileImage
-    }
-
-}
+class Request (
+    var userName: String,
+    var mailRequest: String,
+    var profileImage : String
+    )
--- app/src/main/java/com/example/awesomechat/model/Users.kt
+++ app/src/main/java/com/example/awesomechat/model/Users.kt
@@ -6,33 +6,13 @@ import javax.inject.Inject
 import kotlin.collections.ArrayList
 
 @IgnoreExtraProperties
-class Users {
-    var userName: String? = null
-    var mail: String? = null
-    var password: String? = null
-    var date : String? = null
-    var phone : String?= null
-    var profileImage : String? = null
-    var isMe : String? = null
-
-    constructor(userName: String?, mail: String?,password: String?,date: String?,phone : String?
-                ,profileImage: String?,isMe : String?) {
-        this.userName = userName
-        this.mail = mail
-        this.date = date
-        this.password= password
-        this.phone = phone
-        this.isMe = isMe
-        this.profileImage = profileImage
-    }
-
-    override fun toString(): String {
-        return "Users{" +
-                ", userName='" + userName + '\'' +
-                ", mail='" + mail + '\'' +
-                ", password='" + password + '\'' +
-                '}'
-    }
-
-}
+data class Users (
+    val userName: String,
+    val mail: String,
+    val password: String,
+    val date : String,
+    val phone : String,
+    val profileImage : String,
+    val isMe : String
+    )
 
